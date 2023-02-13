630d8cbc4665a69c343d2d5671f2f365ad4e9be5
Victor H Garcia <ihugo@live.com>
2023-02-13 12:51:12 -0600

    Initial full screen support for the media element.

--- src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.android.cs
+++ src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.android.cs
@@ -1,4 +1,6 @@
-﻿using Android.Content;
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
 using Android.Views;
 using Android.Widget;
 using AndroidX.CoordinatorLayout.Widget;
@@ -12,7 +14,11 @@ namespace CommunityToolkit.Maui.Core.Views;
 /// </summary>
 public class MauiMediaElement : CoordinatorLayout
 {
+	readonly Context context;
 	readonly StyledPlayerView playerView;
+	readonly FrameLayout relativeLayout;
+	int originalUiOptions;
+	int originalHeight;
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="MauiMediaElement"/> class.
@@ -22,14 +28,14 @@ public class MauiMediaElement : CoordinatorLayout
 	public MauiMediaElement(Context context, StyledPlayerView playerView) : base(context)
 	{
 		this.playerView = playerView;
+		this.context = context;
+
+		playerView.FullscreenButtonClick += PlayerView_FullscreenButtonClick;
 
 		// Create a RelativeLayout for sizing the video
-		RelativeLayout relativeLayout = new(context)
+		relativeLayout = new(context)
 		{
-			LayoutParameters = new CoordinatorLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent)
-			{
-				Gravity = (int)GravityFlags.Center
-			}
+			LayoutParameters = new FrameLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent)
 		};
 
 		relativeLayout.AddView(playerView);
@@ -37,22 +43,113 @@ public class MauiMediaElement : CoordinatorLayout
 	}
 
 	/// <summary>
+	/// Allows the video to enter or exist a full screen mode
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	void PlayerView_FullscreenButtonClick(object? sender, StyledPlayerView.FullscreenButtonClickEventArgs e)
+	{
+		// Ensure there is a player view
+		if (this.playerView is null)
+		{
+			return;
+		}
+
+		// Ensure current activity exists
+		var currentActivity = Platform.CurrentActivity;
+		if (currentActivity is null)
+		{
+			return;
+		}
+
+		// Ensure current window is available
+		var currentWindow = currentActivity.Window;
+		if (currentWindow is null)
+		{
+			return;
+		}
+
+		// Ensure current resources exist
+		var currentResources = currentActivity.Resources;
+		if (currentResources is null)
+		{
+			return;
+		}
+
+		
+		if (e.IsFullScreen)
+		{
+			// Get the original values
+			originalHeight = this.playerView.Height;
+
+			// Force the landscape on the device
+			currentActivity.RequestedOrientation = Android.Content.PM.ScreenOrientation.Landscape;
+
+			// Hide the status bar and enter a full screen immerse mode
+			if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.R)
+			{
+				currentWindow.SetDecorFitsSystemWindows(false);
+				currentWindow.InsetsController?.Hide(WindowInsets.Type.NavigationBars());
+			}
+			else
+			{
+				originalUiOptions = (int)currentWindow.DecorView.SystemUiVisibility;
+				var newUiOptions = originalUiOptions | (int)SystemUiFlags.LayoutStable | (int)SystemUiFlags.LayoutHideNavigation | (int)SystemUiFlags.LayoutHideNavigation |
+								(int)SystemUiFlags.LayoutFullscreen | (int)SystemUiFlags.HideNavigation | (int)SystemUiFlags.Fullscreen | (int)SystemUiFlags.Immersive;
+
+				currentWindow.DecorView.SystemUiVisibility = (StatusBarVisibility)newUiOptions;
+			}
+
+			// We update the player layout
+			if (currentWindow.DecorView is FrameLayout)
+			{
+				var currentDisplayMetrics = currentResources.DisplayMetrics;
+				if (currentDisplayMetrics is not null)
+				{
+					var customHeight = currentDisplayMetrics.WidthPixels;
+					var customWidth = currentDisplayMetrics.HeightPixels;
+
+					var frameLayoutParameters = new FrameLayout.LayoutParams(customWidth, customHeight, GravityFlags.CenterHorizontal);
+					this.playerView.LayoutParameters = frameLayoutParameters;
+				}
+			}
+		}
+		else
+		{
+			currentActivity.RequestedOrientation = Android.Content.PM.ScreenOrientation.Portrait;
+
+			if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.R)
+			{
+				currentWindow.SetDecorFitsSystemWindows(false);
+				currentWindow.InsetsController?.Show(WindowInsets.Type.NavigationBars());
+			}
+			else
+			{
+				currentWindow.DecorView.SystemUiVisibility = (StatusBarVisibility)originalUiOptions;
+			}
+
+			var frameLayoutParameters = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, originalHeight);
+			playerView.LayoutParameters = frameLayoutParameters;
+		}
+	}
+
+	/// <summary>
 	/// Releases the unmanaged resources used by the <see cref="MediaElement"/> and optionally releases the managed resources.
 	/// </summary>
 	/// <param name="disposing"><see langword="true"/> to release both managed and unmanaged resources; <see langword="false"/> to release only unmanaged resources.</param>
 	protected override void Dispose(bool disposing)
+{
+	if (disposing)
 	{
-		if (disposing)
+		if (playerView is not null)
 		{
-			if (playerView is not null)
-			{
-				// https://github.com/google/ExoPlayer/issues/1855#issuecomment-251041500
-				playerView.Player?.Release();
-				playerView.Player?.Dispose();
-				playerView.Dispose();
-			}
+			// https://github.com/google/ExoPlayer/issues/1855#issuecomment-251041500
+			playerView.Player?.Release();
+			playerView.Player?.Dispose();
+			playerView.Dispose();
 		}
-
-		base.Dispose(disposing);
 	}
+
+	base.Dispose(disposing);
+}
 }
